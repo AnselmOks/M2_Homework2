1f1dd31be8dd6e1c9d349edca746ffe8969bd488
Andrey Moskvitin <anselm2007@mail.ru>
2026-01-15 18:02:54 +0300

    Module 2 homework 5

--- src/org/skypro/skyshop/service/SearchEngine.java
+++ src/org/skypro/skyshop/service/SearchEngine.java
@@ -26,6 +26,36 @@ public class SearchEngine {
         return foundItems;
     }
 
+    public Searchable searchBestResult(String searchString) throws BestResultNotFound {
+
+        Searchable foundItem = null;
+        int maxInclude = 0;
+
+        for (Searchable item : items) {
+            if (item != null) {
+                String searchItem = item.getSearchItem();
+                int numInclude = 0;
+                int index = 0;
+                int subIndex = searchItem.indexOf(searchString, index);
+                while (subIndex != -1) {
+                    numInclude++;
+                    index = subIndex + searchString.length();
+                    subIndex = searchItem.indexOf(searchString, index);
+                }
+                if (numInclude > maxInclude) {
+                    maxInclude = numInclude;
+                    foundItem = item;
+                }
+            }
+        }
+
+        if (foundItem == null) {
+            throw new BestResultNotFound("Не найден подходящий объект по запросу " + searchString);
+        }
+
+        return foundItem;
+    }
+
     public void add(Searchable item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
